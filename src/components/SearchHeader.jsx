3c68125e4ba7d5c6d7d92749fa161ef6d907bd74
surajkabirath <surajkabirath1234@gmail.com>
2024-06-02 18:14:17 +0545

    searchHeader update

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -23,8 +23,8 @@ export default function SearchHeader() {
         <SearchBoxInside/>
         </div>
         <div className='hidden md:inline-flex space-x-2'>
-          <RiSettings3Line className='bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer' />
-          <TbGridDots className='bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer' />
+          <RiSettings3Line className='bg-transparent hover:bg-gray-800 p-2 text-4xl rounded-full cursor-pointer' />
+          <TbGridDots className='bg-transparent hover:bg-gray-800 p-2 text-4xl rounded-full cursor-pointer' />
         </div>
         <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>
           Sign in
