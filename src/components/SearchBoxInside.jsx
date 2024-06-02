0841cfe1ca7222e96a15444ea34033d371f85474
surajkabirath <surajkabirath1234@gmail.com>
2024-06-02 18:40:41 +0545

    add searchBoxInside

--- src/components/SearchBoxInside.jsx
+++ src/components/SearchBoxInside.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+
+import { RxCross2 } from "react-icons/rx";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { SiGooglelens } from "react-icons/si";
+import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const SearchBoxInside = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  };
   return (
-    <div>SearchBoxInside</div>
-  )
-}
+    <form
+      onSubmit={handleSubmit}
+      className="flex border border-gray-900 rounded-full shadow-lg px-6 py-2 ml-10 mr-5 flex-grow max-w-3xl items-center bg-[#303134]"
+    >
+      <input
+        type="text"
+        className="w-full focus:outline-none bg-transparent"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+      />
+      <RxCross2 
+        className="text-2xl text-gray-500  cursor-pointer sm:mr-2"
+        onClick={() => setTerm("")}
+      />
+      <BsFillMicFill fill="#4285f4" cx="144.07" cy="144" r="16" className="hidden sm:inline-flex text-4xl  border-l-2   mr-4 pl-4" />
+      <SiGooglelens 
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer mr-4"
+        onClick={handleSubmit}
+      />
+      <AiOutlineSearch
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+        onClick={handleSubmit}
+      />
+    </form>
+  );
+};
 
-export default SearchBoxInside
+export default SearchBoxInside;
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { RiSettings3Line } from 'react-icons/ri';
 import { TbGridDots } from 'react-icons/tb';
 import SearchHeaderOptions from './SearchHeaderOptions';
 import SearchBoxInside from './SearchBoxInside';
@@ -13,8 +12,8 @@ export default function SearchHeader() {
           <Image
             src='https://upload.wikimedia.org/wikipedia/commons/9/95/Transparent_google_logo_2015.png'
             alt='Google Logo'
-            width={120}
-            height={40}
+            width={65}
+            height={20}
             priority
             style={{ width: 'auto' }}
           />
@@ -23,8 +22,9 @@ export default function SearchHeader() {
         <SearchBoxInside/>
         </div>
         <div className='hidden md:inline-flex space-x-2'>
-          <RiSettings3Line className='bg-transparent hover:bg-gray-800 p-2 text-4xl rounded-full cursor-pointer' />
+          
           <TbGridDots className='bg-transparent hover:bg-gray-800 p-2 text-4xl rounded-full cursor-pointer' />
+          
         </div>
         <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>
           Sign in
